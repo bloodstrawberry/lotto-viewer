40c990f0dba0e9aefef3df4af65fb0e7cf484be9
bloodstrawberry2 <40860674+bloodstrawberry@users.noreply.github.com>
2025-11-27 00:41:44 +0900

    Revert "delete export"
    This reverts commit 15ecef747b5c8da625643ae4030ebfeca0f556bb.

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,39 @@ import type { NextConfig } from 'next';
 
 // ----------------------------------------------------------------------
 
+/**
+ * Static Exports in Next.js
+ *
+ * 1. Set `isStaticExport = true` in `next.config.{mjs|ts}`.
+ * 2. This allows `generateStaticParams()` to pre-render dynamic routes at build time.
+ *
+ * For more details, see:
+ * https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+ *
+ * NOTE: Remove all "generateStaticParams()" functions if not using static exports.
+ */
 const isStaticExport = true;
 
+// ----------------------------------------------------------------------
+
 const nextConfig: NextConfig = {
-  basePath: '/lotto-viewer', // GitHub Pages 서브 경로
-  assetPrefix: '/lotto-viewer/', // 끝에 '/' 추가
+  basePath: '/lotto-viewer',
+  assetPrefix: '/lotto-viewer',
   trailingSlash: true,
   output: isStaticExport ? 'export' : undefined,
+  env: {
+    BUILD_STATIC_EXPORT: JSON.stringify(isStaticExport),
+  },
+  // Without --turbopack (next dev)
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
     return config;
   },
+  // With --turbopack (next dev --turbopack)
   turbopack: {
     rules: {
       '*.svg': {
