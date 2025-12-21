6ed1c6d7a16caab4129c67943f11589b3a5cfa46
bloodstrawberry2 <40860674+bloodstrawberry@users.noreply.github.com>
2025-12-21 20:55:03 +0900

    Update overview-analytics-view.tsx

--- src/sections/overview/analytics/view/overview-analytics-view.tsx
+++ src/sections/overview/analytics/view/overview-analytics-view.tsx
@@ -28,7 +28,7 @@ export function OverviewAnalyticsView() {
   return (
     <DashboardContent maxWidth="xl">
       <Typography variant="h4" sx={{ mb: { xs: 3, md: 5 } }}>
-        Hi, Welcome back 👋
+        통계분석
       </Typography>
 
       <Grid container spacing={3}>
